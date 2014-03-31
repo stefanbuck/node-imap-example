9119de98e95358c3d88090504ee18d5e72071642
Stefan Buck <web@broud.de>
2014-04-01 00:39:31 +0200

    fetch mails from server

--- index.js
+++ index.js
@@ -13,49 +13,61 @@ function openInbox(cb) {
     imap.openBox('INBOX', false, cb);
 }
 
-imap.once('ready', function() {
-    openInbox(function(err, box) {
+imap.once('ready', function () {
+    openInbox(function (err, box) {
         if (err) throw err;
 
-        var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM SUBJECT DATE)','TEXT'] });
-        var mail;
-        f.on('message', function(msg, seqno) {
+        imap.sort([ '-ARRIVAL' ], [ 'UNSEEN' ], function (err, results) {
+            if (err) throw err;
 
-            msg.on('body', function(stream, info) {
-                var buffer = '';
-                mail = {};
+            if(results.length === 0) {
+                console.log('Nothing to fetch');
+                return imap.end();
+            }
 
-                stream.on('data', function(chunk) {
-                    buffer += chunk.toString('utf8');
+            var f = imap.fetch(results, {
+                markSeen: true,
+                bodies: ['HEADER.FIELDS (FROM SUBJECT DATE)', 'TEXT']
+            });
+            var mail;
+            f.on('message', function (msg, seqno) {
+
+                msg.on('body', function (stream, info) {
+                    var buffer = '';
+                    mail = {};
+
+                    stream.on('data', function (chunk) {
+                        buffer += chunk.toString('utf8');
+                    });
+                    stream.once('end', function () {
+                        if (info.which == 'TEXT') {
+                            mail.body = buffer;
+                        } else if (info.which === 'HEADER.FIELDS (FROM SUBJECT DATE)') {
+                            mail.header = Imap.parseHeader(buffer);
+                        }
+                    });
                 });
-                stream.once('end', function() {
-                    if (info.which == 'TEXT') {
-                        mail.body = buffer;
-                    } else if( info.which === 'HEADER.FIELDS (FROM SUBJECT DATE)') {
-                        mail.header = Imap.parseHeader(buffer);
-                    }
+                msg.once('end', function () {
+                    console.log(mail);
+                    mail = {};
                 });
             });
-            msg.once('attributes', function(attrs) {
-//                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            f.once('error', function (err) {
+                console.log('Fetch error: ' + err);
             });
-        });
-        f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-            console.log('Done fetching all messages!');
-            console.log( mail.header );
-            imap.end();
+            f.once('end', function () {
+                console.log('Done fetching all messages!');
+                imap.end();
+            });
         });
     });
 });
 
-imap.once('error', function(err) {
+imap.once('error', function (err) {
     console.log(err);
 });
 
-imap.once('end', function() {
+imap.once('end', function () {
     console.log('Connection ended');
 });
 
